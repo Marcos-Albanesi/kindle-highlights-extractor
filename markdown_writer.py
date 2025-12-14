164c03b234d7abf2b05ac1acccdb0bfe8203f8a5
Marcos-Albanesi <albanesimarcos@gmail.com>
2025-12-14 10:51:14 -0300

    Mejora en la exportación, agregando fecha de inicio y fin de lectura de cada libro.

--- markdown_writer.py
+++ markdown_writer.py
@@ -6,6 +6,15 @@ def export_markdown_files(highlights_by_book):
 
         content = f"# {title}\n\n"
 
+        first_metadata = highlights[0]["metadata"]
+        last_metadata = highlights[-1]["metadata"]
+
+        first_highlight = first_metadata[first_metadata.rfind("|") + 2:]
+        last_highlight = last_metadata[last_metadata.rfind("|") + 2:]
+
+        content += f"Fecha de inicio: {first_highlight}.\n"
+        content += f"Fecha de fin: {last_highlight}.\n\n"
+
         for item in highlights:
             content += item["highlight"] + "\n"
 
